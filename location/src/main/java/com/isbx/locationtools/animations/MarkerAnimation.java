cff1b88d683776a8dd828ceb86fb3645c0b1ddb0
Alex Crimi <alexscrimi@gmail.com>
2016-08-24 11:41:53 -0700

    Refactors MarkerAnimation to extend Animation and be more configurable

--- location/src/main/java/com/isbx/locationtools/animations/MarkerAnimation.java
+++ location/src/main/java/com/isbx/locationtools/animations/MarkerAnimation.java
@@ -1,40 +1,87 @@
 package com.isbx.locationtools.animations;
 
-/* Copyright 2013 Google Inc.
-   Licensed under Apache 2.0: http://www.apache.org/licenses/LICENSE-2.0.html
-   https://gist.github.com/broady/6314689*/
-
-import android.animation.FloatEvaluator;
-import android.animation.TypeEvaluator;
-import android.animation.ValueAnimator;
-import android.view.animation.LinearInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
-public class MarkerAnimation {
-    public static void animateMarker(final Marker marker, final LatLng finalPosition, final float finalHeading, final LatLngInterpolator latLngInterpolator, long duration) {
-        final TypeEvaluator<LatLng> typeEvaluator = new TypeEvaluator<LatLng>() {
-            @Override
-            public LatLng evaluate(float fraction, LatLng startValue, LatLng endValue) {
-                return latLngInterpolator.interpolate(fraction, startValue, endValue);
-            }
-        };
+/**
+ * An implementation of {@link Animation} to animate both the position and rotation of a Google
+ * Maps {@link Marker}.
+ */
+public class MarkerAnimation extends Animation {
 
-        final FloatEvaluator floatEvaluator = new FloatEvaluator();
-        final LatLng startPosition = marker.getPosition();
-        final Float startRotation = marker.getRotation();
+    private Marker marker;
+    private LatLng startPosition;
+    private LatLng endPosition;
+    private float startRotation;
+    private float endRotation;
 
-        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
-        animator.setDuration(duration);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                marker.setPosition(typeEvaluator.evaluate(valueAnimator.getAnimatedFraction(), startPosition, finalPosition));
-                marker.setRotation(floatEvaluator.evaluate(valueAnimator.getAnimatedFraction(), startRotation, finalHeading));
-            }
-        });
-        animator.start();
+    private LatLngInterpolator latLngInterpolator;
+
+    /**
+     * Creates a new MarkerAnimation that will animate both the position and rotation of the given
+     * marker.
+     *
+     * @param marker The {@link Marker} to animate
+     * @param endPosition The final {@link LatLng} position of the marker
+     * @param endRotation The final rotation of the marker in degrees
+     *
+     * @see Marker#setPosition(LatLng)
+     * @see Marker#setRotation(float)
+     */
+    public MarkerAnimation(Marker marker, LatLng endPosition, float endRotation) {
+        this.marker = marker;
+        this.startPosition = marker.getPosition();
+        this.startRotation = marker.getRotation();
+        this.endPosition = endPosition;
+        this.endRotation = endRotation;
+
+        latLngInterpolator = new LatLngInterpolator.LinearFixed();
+    }
+
+    /**
+     * Creates a new MarkerAnimation that will animate the position of the given marker.
+     *
+     * @param marker The {@link Marker} to animate
+     * @param endPosition The final {@link LatLng} position of the marker
+     *
+     * @see Marker#setPosition(LatLng)
+     */
+    public MarkerAnimation(Marker marker, LatLng endPosition) {
+        this(marker, endPosition, marker.getRotation());
+    }
+
+    /**
+     * Creates a new MarkerAnimation that will animate the rotation of the given marker.
+     *
+     * @param marker The {@link Marker} to animate
+     * @param endRotation The final rotation of the marker in degrees
+     *
+     * @see Marker#setRotation(float)
+     */
+    public MarkerAnimation(Marker marker, float endRotation) {
+        this(marker, marker.getPosition(), endRotation);
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        if (startPosition != endPosition) {
+            marker.setPosition(latLngInterpolator.interpolate(interpolatedTime, startPosition, endPosition));
+        }
+        marker.setRotation(startRotation + (endRotation - startRotation)*interpolatedTime);
+    }
+
+    /**
+     * Sets the {@link LatLngInterpolator} this animation will use for animating the position of
+     * the marker. Defaults to {@link com.isbx.locationtools.animations.LatLngInterpolator.LinearFixed}.
+     *
+     * @param latLngInterpolator A {@link LatLngInterpolator} to use for position animation
+     *
+     * @see LatLngInterpolator
+     */
+    public void setLatLngInterpolator(LatLngInterpolator latLngInterpolator) {
+        this.latLngInterpolator = latLngInterpolator;
     }
 }
